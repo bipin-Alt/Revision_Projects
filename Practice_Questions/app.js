bcd1d6e78a9f788f207f6a23b1f32d6db52abb67
bipin-Alt <bipin.alt.ev7@gmail.com>
2025-09-16 01:24:24 +0545

    Second question completed

--- Practice_Questions/app.js
+++ Practice_Questions/app.js
@@ -1,6 +1,6 @@
 
 
-//Given an array of numbers, use destructuring to extract the first two numbers and use map to square all of them.
+//Q.N.1 Given an array of numbers, use destructuring to extract the first two numbers and use map to square all of them.
  
 let numbers = [1,23,4 ,5,6];
 const squareTwo = ([a,b]) =>{
@@ -16,3 +16,16 @@ console.log(output);
 };
 squareTwo(numbers);
 
+// Q.N.2 From an array of names, destructure the first name and use filter to keep only names starting with that letter.
+
+const  names =["Ram", "Hari", "Sita", "Joshef","Alspin","Rajesh"];
+const [a] = names;
+const nameSplit = a.split("");
+let firstLetter = nameSplit[0];
+const arrayofMatchingFirstLetter=names.filter((name)=>{
+    const namesSplit= name.split("");
+    if(namesSplit[0]===firstLetter){
+        return name;
+    }
+}) 
+console.log(arrayofMatchingFirstLetter);
