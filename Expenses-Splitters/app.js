b703b35c7e0beab227cc5bd9ddf6aad23af34e26
bipin-Alt <bipin.alt.ev7@gmail.com>
2025-09-17 22:00:22 +0545

    Expenses Tailwind Styling completed

---
+++ Expenses-Splitters/app.js
@@ -0,0 +1,7 @@
+const PayerName = document.getElementById("payer'sName").value;
+const phoneNumber = document.getElementById("");
+const Amount = document.getElementById("");
+const friends = [ ];
+const addNewFriend = (name, phone)=>{
+
+}
